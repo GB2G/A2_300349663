92c6b68ac2ba9bb178c276a6529a6ee713aa3138
Kevin El-Saikali <kev2005@outlook.com>
2023-09-29 13:57:09 -0400

    Finished function 1

--- a2_part_2_300349663.py
+++ a2_part_2_300349663.py
@@ -1,5 +1,9 @@
 def min_enclosing_rectangle(radius, x, y):
-    pass
+    if radius < 0:
+        return None
+    
+    else:
+        return "(" + str(x-radius) + ", " + str(y-radius) + ")"
 
 def vote_percentage(results):
     pass
